3e9116dcb10ad4b92bcc901cd0096df6b4093e2b
Valentin Werner <valwerner2@gmail.com>
2025-11-07 19:10:30 +0100

    fan control curve

--- wirelessDuctFan/src/main.cpp
+++ wirelessDuctFan/src/main.cpp
@@ -5,13 +5,18 @@
 const int PWM_FAN_OUTPUT = 2;
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+float targetTemp = 25.;
+float targetHum = 0.;
+
+enum MODE {MODE_HUM_DOWN, MODE_HUM_UP, MODE_TEMP_DOWN, MODE_TEMP_UP, MODE_SLAVE};
+int currentMode = MODE_TEMP_DOWN;
+
+int maxSpeed = 50;
 
 void setFanSpeed(uint8_t speed)
 {
-    speed = speed % (101 + 1);
-
+    speed = speed > maxSpeed ? maxSpeed : speed;
     speed = !speed ? 0 : map(speed, 1, 100, 25, 255);
-
     analogWrite(PWM_FAN_OUTPUT,speed);
 }
 
@@ -31,6 +36,58 @@ void sensorSetup()
     delay(1000);
 }
 
+int calcFanSpeed(float diff, bool down)
+{
+    const float base = 10.f;
+    if (down && diff > 0.f)
+    {
+        return static_cast<int>(diff * base + pow(diff, 2));
+    }
+    if (!down && diff < 0.f)
+    {
+        diff = abs(diff);
+        return static_cast<int>(diff * base + pow(diff, 2));
+    }
+    return 0;
+}
+
+void updateFanSpeed()
+{
+    sensors_event_t humidity, temp;
+    sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+
+    float difTemp = targetTemp == 0.f ? 0.f : temp.temperature - targetTemp;
+    float difHum = targetHum == 0.f ? 0.f : humidity.relative_humidity - targetHum;
+
+    Serial.printf("\nTemperature: %2.2f -> %2.2f | %2.2f \n", temp.temperature, targetTemp, difTemp);
+    Serial.printf("Humidity   : %2.2f -> %2.2f | %2.2f \n", humidity.relative_humidity, targetHum, difHum);
+
+
+    int newSpeed = 0;
+    switch (currentMode)
+    {
+        case MODE_HUM_DOWN:
+            newSpeed = calcFanSpeed(difHum, true);
+            break;
+        case MODE_HUM_UP:
+            newSpeed = calcFanSpeed(difHum, false);
+            break;
+        case MODE_TEMP_DOWN:
+            newSpeed = calcFanSpeed(difTemp, true);
+            break;
+        case MODE_TEMP_UP:
+            newSpeed = calcFanSpeed(difTemp, false);
+            break;
+        case MODE_SLAVE:
+            newSpeed = 100;
+            break;
+        default:
+            break;
+    }
+    Serial.println("New speed: "+ String(newSpeed));
+    setFanSpeed(newSpeed);
+}
+
 void setup() {
     Serial.begin(115200);
     delay(1000);
@@ -39,9 +96,6 @@ void setup() {
 }
 
 void loop() {
-    sensors_event_t humidity, temp;
-    sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-
-    Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-    Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+    updateFanSpeed();
+    delay(1000);
 }
