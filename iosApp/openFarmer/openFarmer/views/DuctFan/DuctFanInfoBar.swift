51072fc9f5fc4527296765549435fe897de2c88e
Valentin Werner <valwerner2@gmail.com>
2025-11-25 20:14:04 +0100

    name (optional) and mode

--- iosApp/openFarmer/openFarmer/views/DuctFan/DuctFanInfoBar.swift
+++ iosApp/openFarmer/openFarmer/views/DuctFan/DuctFanInfoBar.swift
@@ -10,34 +10,55 @@ import SwiftUI
 
 struct DuctFanInfoBar: View {
     let ductFanCurrent: DuctFan
+    let displayName: Bool = false
+    
+    @Environment(\.editMode) private var editMode
     
     var body: some View {
-        HStack{
-            Label {
-                Text(String(format: "%.02f", ductFanCurrent.info.currentTemp))
-            } icon: {
-                TemperatureIconView(
-                    current: ductFanCurrent.info.currentTemp,
-                    target: ductFanCurrent.info.currentTargetTemp
-                )
+        VStack(alignment: .leading,){
+            if(displayName)
+            {
+                HStack{
+                    Text(ductFanCurrent.name).bold()
+                    Spacer()
+                    Text(ductFanCurrent.purpose)
+                }.padding(.bottom, paddingSection)
             }
-            Spacer()
-            Label {
-                Text(String(format: "%.02f", ductFanCurrent.info.currentHum))
-            } icon: {
-                HumidityIconView(
-                    current: ductFanCurrent.info.currentTemp,
-                    target: ductFanCurrent.info.currentTargetTemp
-                )
+            HStack{
+                Label {
+                    Text(String(format: "%.02f", ductFanCurrent.info.currentTemp))
+                } icon: {
+                    TemperatureIconView(
+                        current: ductFanCurrent.info.currentTemp,
+                        target: ductFanCurrent.info.currentTargetTemp
+                    )
+                }
+                Spacer()
+                Label {
+                    Text(String(format: "%.02f", ductFanCurrent.info.currentHum))
+                } icon: {
+                    HumidityIconView(
+                        current: ductFanCurrent.info.currentTemp,
+                        target: ductFanCurrent.info.currentTargetTemp
+                    )
+                }
+                Spacer()
+                Label(String(ductFanCurrent.info.currentSpeed) ,systemImage: "fan")
+                Spacer()
+                Image(systemName: ductFanCurrent.info.isDayTime ? "sun.max" : "moon.zzz")
+                    .foregroundStyle(.tint)
+                Spacer()
+                Image(systemName: ductFanCurrent.info.isLoudTime ? "speaker.wave.2" : "speaker.slash")
+                    .foregroundStyle(.tint)
+            }.padding(.bottom, paddingSection)
+            if editMode?.wrappedValue.isEditing != true {
+                HStack()
+                {
+                    Label("Mode", systemImage: "gear")
+                    Spacer()
+                    Text(ductFanCurrent.info.currentMode.label)
+                }
             }
-            Spacer()
-            Label(String(ductFanCurrent.info.currentSpeed) ,systemImage: "fan")
-            Spacer()
-            Image(systemName: ductFanCurrent.info.isDayTime ? "sun.max" : "moon.zzz")
-                .foregroundStyle(.tint)
-            Spacer()
-            Image(systemName: ductFanCurrent.info.isLoudTime ? "speaker.wave.2" : "speaker.slash")
-                .foregroundStyle(.tint)
         }
     }
 }
